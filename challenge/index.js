8cdaf6af867dd72204934a08883eb9df9834ae5d
Chantal Goethals <chantalgoethals@gmail.com>
2022-04-18 20:27:57 -0600

    Session 5 Book Titles

---
+++ challenge/index.js
@@ -0,0 +1,15 @@
+# **5 Book Titles**
+
+Little Red Book of Selling, The Little Book of Talent, JavaScript & JQuery, Battle Royale, Battle Royale
+
+```JAVASCRIPT
+var haveRead = true;
+var booksRead = 4
+var bookType = ("Fiction", "Non-Fiction", "Self Help");
+var maleAuthor = true;
+var hardBack = true;
+var receivedMethod = ("Gift", "School", "Street Library");
+
+
+var harryPotter series = ('book1','book2', 'book3', 'book4', 'book5')
+// Harry Potter Series 
